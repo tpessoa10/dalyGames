88c830f53d9d3781da6a0cd4d7e43bbe7546e73e
thiagopessoa <thiagopessoa@sefaz.gov.pi.br>
2025-07-31 11:59:42 -0300

    Criando componente de jogo do dia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { Container } from "@/components/container";
 import Image from "next/image";
+import { gameProps } from '@/utils/types/game'
+import Link from "next/link";
 
-export default function Home() {
+async function getDalyGame() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    return res.json()
+  } catch (err) {
+    throw new Error("Failed to fetch data")
+  }
+}
+
+export default async function Home() {
+  const dalyGame: gameProps = await getDalyGame();
+
   return (
-   <h1>TESTE</h1>
+    <main className="flex w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8">Separamos um jogo exclusivo para você</h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image src={dalyGame.image_url} alt={dalyGame.title} priority quality={100} width={100} height={100}/>
+          </section>
+        </Link>
+      </Container>
+    </main>
   );
 }
---
+++ src/utils/types/game.ts
@@ -0,0 +1,9 @@
+export interface gameProps{
+    id: number;
+    title: string;
+    description: string;
+    image_url: string;
+    platforms: string[];
+    categories: string[];
+    release: string[];
+}
