5c1e488054355211590a144b69fe23c380a82203
thiagopessoa <thiagopessoa@sefaz.gov.pi.br>
2025-08-01 11:47:44 -0300

    Concluindo recomendação de jogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import { Container } from "@/components/container";
 import Image from "next/image";
 import { gameProps } from '@/utils/types/game'
 import Link from "next/link";
+import {BsArrowRightSquare} from 'react-icons/bs'
 
 async function getDalyGame() {
   try {
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {next:{revalidate: 320}})
     return res.json()
   } catch (err) {
     throw new Error("Failed to fetch data")
@@ -16,12 +17,19 @@ export default async function Home() {
   const dalyGame: gameProps = await getDalyGame();
 
   return (
-    <main className="flex w-full">
+    <main className="w-full">
       <Container>
-        <h1 className="text-center font-bold text-xl mt-8">Separamos um jogo exclusivo para você</h1>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você</h1>
         <Link href={`/game/${dalyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image src={dalyGame.image_url} alt={dalyGame.title} priority quality={100} width={100} height={100}/>
+            <div className="w-full  max-h-96 h-96 relative rounded-lg ">
+              <div className="absolute font-bold text-xl text-white z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p>{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#fff"/>
+              </div>
+              <Image src={dalyGame.image_url} alt={dalyGame.title} priority quality={100} fill={true}
+                className="object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw" />
+            </div>
           </section>
         </Link>
       </Container>
