ea8660e6c54589ca9fcd2e389d9aed424b95119d
thiagopessoa <thiagopessoa@sefaz.gov.pi.br>
2025-08-04 09:47:58 -0300

    Metadados do projeto

--- src/app/game/search/[title]/page.tsx
+++ src/app/game/search/[title]/page.tsx
@@ -5,7 +5,8 @@ import { GameCard } from "@/components/gameCard";
 
 async function getData(title: string) {
     try {
-        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`)
+        const decodeTitle = decodeURI(title)
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&title=${decodeTitle}`)
         return res.json();
     } catch (err) {
         return null
