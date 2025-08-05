e8944863579e670d9fd3f34a10f8bc0383a1d2f3
thiagopessoa <thiagopessoa@sefaz.gov.pi.br>
2025-08-05 11:03:34 -0300

    Criando perfil estático

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,27 @@
+import { Container } from "@/components/container";
+import userImg from '../../../public/user.png'
+import Image from "next/image";
+import { FaShareAlt } from "react-icons/fa";
+
+export default function Profile(){
+    return(
+        <main className="w-full text-black">
+            <Container>
+                <section className="mt-8 mb-6 flex flex-col items-center justify-between relative gap-3 sm:flex-row">
+                    <div className="w-full flex items-center gap-4 text-lg flex-col sm:flex-row justify-center sm:justify-normal">
+                        <Image className="rounded-full w-56 h-56 object-cover" src={userImg} alt="Imagem do usuário"/>
+                        <h1 className="font-bold text-2xl">Usuário</h1>
+                    </div>
+                    <div className="sm:absolute top-0 right-0 gap-3 flex items-center justify-center">
+                        <button className="bg-gray-700 px-4 py-3 rounded-lg text-white">
+                            Configurações
+                        </button>
+                        <button className="bg-gray-700 px-4 py-3 rounded-lg">
+                            <FaShareAlt size={24} color="#fff"/>
+                        </button>
+                    </div>
+                </section>
+            </Container>
+        </main>
+    )
+}
